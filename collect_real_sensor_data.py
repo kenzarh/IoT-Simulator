c591d7a98a446ada0247fee10e7cc9855513a384
kenzarh <kenzariahi98@gmail.com>
2021-03-21 13:22:02 +0100

    Add files via upload

--- collect_real_sensor_data.py
+++ collect_real_sensor_data.py
@@ -8,19 +8,63 @@ parameters = pika.ConnectionParameters("localhost",5672,'/',credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-import json
 from urllib.request import urlopen
+import json
 
-url1 = "http://dafflon.fr:5000/api/Sensor/light/data/LIGHT"
-url2 = "http://dafflon.fr:5000/api/Sensor/lm358/data/SOUND"
+url_sensors = "http://dafflon.fr:5000/api/listSensor"
+sensors = json.loads(urlopen(url_sensors).read())['SensorList']
+
+def metadata (data,time,name,type,url):
+  metadata = {
+  "sensor":{
+  "name":name,
+  "Category":[
+  "located",
+  "real",
+  "..."
+  ],
+  "Type":type,
+  "Hz":1,
+  "DataType":"Integer",
+  "Location":{
+    },
+    "url":url,
+    "port":5000
+  },
+  "data":{
+  "Timestamp":time,
+  "Data":data,
+  "Trust Factor":1,
+  "Scale Factor":1
+  }
+  }
+
+  return metadata
+
+def light():
+  url = "http://dafflon.fr:5000/api/Sensor/light/data/LIGHT"
+  endpoint = '/api/Sensor/light/data/LIGHT'
+  response = json.loads(urlopen(url).read())
+  data = response['data-light-intensity']
+
+  import time
+  import datetime
+  date_string = response['time']
+  date = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+  time = time.mktime(date.timetuple())
+
+  name = [sensor['name'] for sensor in sensors if sensor['endpoint']==endpoint]
+
+  type = 'brightness'
+
+  return data,time,name,type,url
+
+
 
 while True:
-        response1 = urlopen(url1)
-        data1 = json.loads(response1.read())
+        
+        data,time,name,type,url = light()
+        metadata = metadata (data,time,name,type,url)
         channel.basic_publish(exchange='amq.topic', routing_key='light',body=json.dumps(data1))
 
-        response2 = urlopen(url2)
-        data2 = json.loads(response2.read())
-        channel.basic_publish(exchange='amq.topic', routing_key='sound',body=json.dumps(data2))
-       
         time.sleep(5)
