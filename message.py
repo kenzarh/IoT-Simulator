5be4db2256d92e69de83846fc03a5c75f01d64c1
kenzarh <kenzariahi98@gmail.com>
2021-04-10 14:36:34 +0100

    Define XML format for data exchange

---
+++ message.py
@@ -0,0 +1,42 @@
+def send_data (typeObject,idObject,idSensor,sensorCategory, sensorType,frequency,sensorLocation,time,unit,value):
+
+        # Importing the generic XML file
+        import xml.etree.ElementTree as ET
+        tree = ET.parse('C:\\Users\\dell\\Desktop\\IoT Simulator\\sensor_model\\messages_format.xml')
+        root = tree.getroot()
+
+        # Changing the parameters
+
+        element1 = root[0]
+        element1.set('type', typeObject)
+
+        element2 = root[0][0]
+        element2.text=idObject
+
+        element3 = root[0][1][0][0][0]
+        element3.text=idSensor
+
+        element4 = root[0][1][0][1][0]
+        element4.text=sensorCategory
+
+        element5 = root[0][1][0][2][0]
+        element5.text=sensorType
+
+        element6 = root[0][1][0][3][0]
+        element6.text=str(frequency)
+
+        element7 = root[0][1][0][4][0]
+        element7.text=sensorLocation
+
+        element8 = root[0][2][0][0][0]
+        element8.text=str(time)
+
+        element9 = root[0][2][0][2][0]
+        element9.text=unit
+
+        element10 = root[0][2][1]
+        element10.text=str(value)
+
+        message = ET.tostring(root).decode()
+
+        return message
---
+++ sensor_model.py
@@ -0,0 +1,45 @@
+# Importing the CNN model
+import tensorflow as tf 
+model = tf.keras.models.load_model("C:\\Users\\dell\\Desktop\\IoT Simulator\\sensor_model\\CNN.model")
+
+# Importing data (will be changed to collecting data from real sensors)
+import pandas as pd
+df = pd.read_csv("C:\\Users\\dell\\Desktop\\IoT Simulator\\sensor_model\\data.csv")
+columns = ['T (degC)','p (mbar)','H2OC (mmol/mol)','rho (g/m**3)']
+data = df[columns]
+data = data.values
+Y = data[:,0] # Temperatures
+X = data [:,1:] # Features used to predict temperature 
+X = X.reshape(X.shape[0], X.shape[1], 1)
+
+
+# Sending the message to the RabbitMQ Broker
+import pika # The RabbitMQ client library for Python
+credentials = pika.PlainCredentials('admin', 'admin')
+parameters = pika.ConnectionParameters("localhost",5672,'/',credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+import time
+import datetime
+from message import send_data
+import xml.etree.ElementTree as ET
+
+i = 1000 # Starting whith the raw i
+
+while True:
+
+        f = 5 # Messages frequency
+
+        # Predicting the temperature
+        temperature = int(model.predict (X[i:i+1,:]))
+
+        now = datetime.datetime.now().timestamp()
+        
+        message = send_data(typeObject="Temperature",idObject="VirtualTemperature1",idSensor="VirtualTemperature1",sensorCategory="virtual", sensorType="temperature",frequency=f,sensorLocation="virtual",time=now,unit="◦C",value=temperature)
+
+        channel.basic_publish(exchange='amq.topic', routing_key='temperature',body=message)
+
+        i = i + 1
+
+        time.sleep(f)
